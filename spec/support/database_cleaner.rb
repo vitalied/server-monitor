bd40bb53ebc4f2eb89e42a9081aad6455ca0c103
Vitalie D <vitalie.dev@gmail.com>
2016-04-23 11:35:07 +0300

    database_cleaner config

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,19 @@
+RSpec.configure do |config|
+
+  config.use_transactional_fixtures = false
+
+  # Cleanup the DB in between test runs
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
